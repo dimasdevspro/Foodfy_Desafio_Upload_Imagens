d8a7e3f767b5f8762762e20569c3eca43319a8fb
dimasdevspro <dimasdevspro@github.com>
2020-12-09 11:33:31 -0300

    update upload by image

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = express.Router()
 const recipes = require('./app/controllers/recipes')
 const chefs = require('./app/controllers/chefs')
+const multer = require('./app/middlewares/multer')
 //VIEWS
 
 routes.get("/", recipes.home);
@@ -15,8 +16,8 @@ routes.get("/admin/recipes/recipes/create", recipes.create); // Mostrar formulá
 routes.get("/admin/recipes/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 routes.get("/admin/recipes/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
-routes.post("/admin/recipes/recipes", recipes.post); // Cadastrar nova receita
-routes.put("/admin/recipes/recipes", recipes.put); // Editar uma receita
+routes.post("/admin/recipes/recipes", multer.array("photos", 5), recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes/recipes", multer.array("photos", 5), recipes.put); // Editar uma receita
 routes.delete("/admin/recipes/recipes", recipes.delete); // Deletar uma receita
 
 //ADMIN CHEFS
